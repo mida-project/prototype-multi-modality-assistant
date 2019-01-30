306ebcad487177de7fb4d12409d8230cc0c7caa7
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2019-01-30 16:11:00 +0000

    [REFACTOR] Client Index

--- src/client/index.js
+++ src/client/index.js
@@ -20,21 +20,50 @@ loadTemplate(studyViewerPath, function(element) {
   studyViewerTemplate = element;
 });
 
+/* ================================================== */
+/*
+ * Definition of HTML Tag Variables
+ */
+
+var v001 = '<tr><td>';
+var v002 = '</td><td>';
+var v003 = '</tr>';
+var v004 = '<li><div id=complete-tab><a href="#x';
+var v005 = '" data-toggle="tab">';
+var v006 = '</a>';
+var v007 = '<input type="button" class="closeBtn" value="X" />';
+var v008 = '</li></div>';
+var v009 = " <br />";
+var v010 = " <span style='color:red'>";
+var v011 = " ";
+var v012 = "% </span> ";
+var v013 = "!! <br />";
+var v014 = ": <span style='color:red'>";
+var v015 = "</span>";
+var v016 = "Sorry i have no infomation about the patient! <br />";
+var v017 = " <span />";
+
+/* ================================================== */
+
 // Get study list from JSON manifest
 $.getJSON(studyListFile, function(data) {
   //console.log("Getting study list...");
   if (typeof data.studyList === "object") {
     //console.log("Consuming study list...");
     data.studyList.forEach(function(study) {
+      var c001 = study.patientId;
+      var c002 = study.studyDate;
+      var c003 = study.modality;
+      var c004 = study.studyDescription;
+      var c005 = study.numImages;
+      var c006 = study.studyId
       //console.log("Creating study list tables...");
       // Create one table row for each study in the manifest
-      var studyRow = '<tr><td>' +
-        study.patientId + '</td><td>' +
-        study.studyDate + '</td><td>' +
-        study.modality + '</td><td>' +
-        study.studyDescription + '</td><td>' +
-        study.numImages + '</td><td>' +
-        '</tr>';
+      var p001 = v001 + c001 + v002;
+      var p002 = c002 + v002 + c003;
+      var p003 = v002 + c004 + v002;
+      var p004 = c005 + v002 + v003;
+      var studyRow = p001 + p002 + p003 + p004;
 
       // Append the row to the study list
       var studyRowElement = $(studyRow).appendTo('#studyListData');
@@ -46,31 +75,32 @@ $.getJSON(studyListFile, function(data) {
         } else {
 
           // Add new tab for this study and switch to it
-          var studyTab = '<li><div id=complete-tab><a href="#x' + study.patientId + '" data-toggle="tab">' + study.patientId + '</a>' +
-            '<input type="button" class="closeBtn" value="X" />' + '</li></div>';
+          var studyTab = v004 + c001 + v005 + c001 + v006 + v007 + v008;
 
           // JSON File Path
-          var jsonFileUrl = "../common/dataset/" + study.patientId +".json";
+          var jsonFileUrl = "../common/dataset/" + c001 + ".json";
           var messageUrl = "../common/messages/assistant_msg.json";
           $.getJSON(jsonFileUrl)
             .done(function(individualPatientData) {
-                // informations reading from json file
-                let biradsAssis = individualPatientData.patient[0].biradsAssis;
-                let biradsPhys = individualPatientData.patient[0].biradsPhys;
-                let cancerChance = individualPatientData.patient[0].cancerChance; 
-                $.getJSON(messageUrl)
-                  .done(function(messageText) {
-                      let msg1 = messageText.assisMessage[0].message_001;
-                      let msg2 = messageText.assisMessage[0].message_002;
-                      let msg3 = messageText.assisMessage[0].message_003;
-                      let msg4 = messageText.assisMessage[0].message_004;
-                      let msg5 = messageText.assisMessage[0].message_005;
-                    $('.assistant_information').css({display:"block"});
-                    let assistantText = msg1 +" "+ msg2 + " <br />" + msg3 + " <span style='color:red'>"+ cancerChance +"% </span> " + msg4 + "!! <br />" + msg5 +": <span style='color:red'>" + biradsPhys+ "</span>"; 
-                    // display text if any data for the patient 
-                    document.getElementById("assistant_information").innerHTML = assistantText;
-                    avator_animation();
-                  })
+              // informations reading from json file
+              let biradsAssis = individualPatientData.patient[0].biradsAssis;
+              let biradsPhys = individualPatientData.patient[0].biradsPhys;
+              let cancerChance = individualPatientData.patient[0].cancerChance; 
+              $.getJSON(messageUrl)
+                .done(function(messageText) {
+                  let msg1 = messageText.assisMessage[0].message_001;
+                  let msg2 = messageText.assisMessage[0].message_002;
+                  let msg3 = messageText.assisMessage[0].message_003;
+                  let msg4 = messageText.assisMessage[0].message_004;
+                  let msg5 = messageText.assisMessage[0].message_005;
+                  $('.assistant_information').css({display:"block"});
+                  var p005 = msg1 + v011 + msg2 + v009 + msg3 + v010;
+                  var p006 = v012 + msg4 + v013 + msg5 + v014;
+                  let assistantText = p005 + cancerChance + p006 + biradsPhys + v015;
+                  // display text if any data for the patient 
+                  document.getElementById("assistant_information").innerHTML = assistantText;
+                  avator_animation();
+                })
 
               })
               .fail(function(jqXHR, textStatus) {
@@ -81,7 +111,7 @@ $.getJSON(studyListFile, function(data) {
                     let warn2 = messageText.assisWarning[0].warning_002;
                     let warn3 = messageText.assisWarning[0].warning_003;
                     $('.assistant_information').css({display:"block"});
-                    let failAssistantText = "Sorry i have no infomation about the patient! <br />" + warn1 +" <span />" + warn2 + warn3;
+                    let failAssistantText = v016 + warn1 + v017 + warn2 + warn3;
                     document.getElementById("assistant_information").innerHTML = failAssistantText;
                     avator_animation();
                   })
@@ -96,7 +126,7 @@ $.getJSON(studyListFile, function(data) {
           studyViewerCopy.find('.imageViewer').append(viewportCopy);
 
  
-          studyViewerCopy.attr("id", 'x' + study.patientId);
+          studyViewerCopy.attr("id", 'x' + c001);
           // Make the viewer visible
           studyViewerCopy.removeClass('hidden');
           // Add section to the tab content
@@ -111,8 +141,8 @@ $.getJSON(studyListFile, function(data) {
           });
 
           studyViewerCopy.roiData = {
-            studyId: study.studyId,
-            modality: study.modality,
+            studyId: c006,
+            modality: c003,
             stacks: [],
           };
 
@@ -132,7 +162,7 @@ $.getJSON(studyListFile, function(data) {
           });
 
           // Now load the study.json
-          loadStudy(studyViewerCopy, viewportTemplate, study.studyId + fileFormat);
+          loadStudy(studyViewerCopy, viewportTemplate, c006 + fileFormat);
         }
       });
     });
@@ -140,10 +170,7 @@ $.getJSON(studyListFile, function(data) {
   }
 });
 
-
-
 // Resize main
-
 
 // Show tabs on click
 $('#tabs a').click (function(e) {
@@ -160,7 +187,7 @@ function resizeMain() {
 
 // Call resize main on window resize
 $(window).resize(function() {
-    resizeMain();
+  resizeMain();
 });
 resizeMain();
 
